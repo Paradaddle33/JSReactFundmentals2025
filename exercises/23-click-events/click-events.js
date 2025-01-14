c4b067eb2b481b8006b9bc59ee28f23340d2b1cb
Paradaddle33 <74434299+Paradaddle33@users.noreply.github.com>
2025-01-14 15:34:53 -0600

    completed excersize click-events

--- exercises/23-click-events/click-events.js
+++ exercises/23-click-events/click-events.js
@@ -8,7 +8,12 @@
    * When the clicks on the button that says "Alert Me!", it should display an alert.
    * @see https://developer.mozilla.org/en-US/docs/Web/API/Window/alert
    */
-  // Write your answer here
+  const alertButton = document.getElementById('alert-button');
+  alertButton.addEventListener("click", () => {
+    alert("Hello World!");
+  });
+
+
   /**
    * Problem 2: Disable a button that will charge a credit card.
    *
@@ -16,5 +21,9 @@
    * 1. Disable the button when it is clicked.
    * 2. Change the text to say e.g. "Loading ..." once it is clicked.
    */
-  // Write your answer here
+  const disButton = document.getElementById('creditCard');
+  disButton.addEventListener("click", () => {
+    disButton.disabled = true;
+    disButton.innerText = "Loading ...";
+  });
 })();
